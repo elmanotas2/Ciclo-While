fd7ed011b305fb82d6069ba8c62c7a04daebbcc4
soto.16 <160264828+elmanotas2@users.noreply.github.com>
2024-02-21 08:14:52 -0600

    Suma de numeros del 1 al 100 con while

---
+++ whilee4.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+main ()
+{
+
+    int i= 1;
+    int suma=0;
+
+    while(i<=100)
+    suma = suma + i;
+    i++;
+
+    printf("la suma del 1 al 100 es: %d", suma);
+
+}
+
+
+
+
+
+
+
+
